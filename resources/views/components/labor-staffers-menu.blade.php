583e6396d247935e11925a7d53707b0857ffd55c
burrodecarga <edwinedwinhenriquezh@hotmail.com>
2021-06-16 22:42:34 -0400

    master

--- resources/views/components/labor-staffers-menu.blade.php
+++ resources/views/components/labor-staffers-menu.blade.php
@@ -81,17 +81,32 @@
         <ul>
             <li><a class="text-white" href="{{url('/dashboard')}}">Home</a></li>
             <li><a class="text-white" href="{{ url('/about')}} ">About us</a></li>
-            <li><a class="text-white" href="./jobs/">Jobs</a></li>
+            @hasanyrole('candidate')
+            <li><a class="text-white" href="/offers">Jobs</a></li>
+            @endhasanyrole
+            @hasanyrole('candidate')
             <li><a class="text-white" href="{{url('/candidates')}}">Candidates</a></li>
+            @endhasanyrole
+            @hasanyrole('employer')
             <li><a class="text-white" href="{{url('/employers')}}">Employers</a></li>
+            @endhasanyrole
+            @hasanyrole('candidate|employer')
             <li><a class="text-white" href="{{url('/services')}}">Services</a></li>
             <li><a class="text-white" href="{{url('/contact')}}">Contact us</a></li>
-            @auth
+            @endhasanyrole
+              @auth
             <li><a class="text-white" href="{{route('profile.show')}}">perfil</a></li>
-            <li><a class="text-white" href="{{route('user-profile')}}">profile</a></li>
+            @hasanyrole('candidate')
+            <li><a class="text-white mr-4" href="{{route('user-profile')}}">profile</a></li>
+            @endhasanyrole
+            @hasanyrole('employer')
+            <li><a class="text-white mr-4" href="{{route('employer-posts')}}">My Offers</a></li>
+            @endhasanyrole
+
+            @hasanyrole('admin|super-admin')
             <li><a class="text-white" href="{{route('admin')}}">admin</a></li>
-            <li>
-            <form method="POST" action="{{ route('logout') }}">
+<li>           @endhasanyrole
+            <form method="POST" action="{{ route('logout') }}" class="ml-4">
                 @csrf
 
                 <a class="text-white" href="{{ route('logout') }}"
